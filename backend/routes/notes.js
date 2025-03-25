8a4ccc767202b3e2de713ceb3ea2c7a9767cd875
Mikita.Shah <mikitashah2009@gmail.com>
2025-03-25 20:17:24 +0530

    Update the existing note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -10,11 +10,11 @@ router.get("/fetchallnotes", fetchuser, async (req, res) => {
     const notes = await Notes.find({ user: req.user.id });
     res.json(notes);
   } catch (error) {
-    res.status(401), send({ error: "Please authenticate using a valid token" });
+    res.status(401).send({ error: "Please authenticate using a valid token" });
   }
 });
 
-// ROUTE 2: ADD A NEW NOTE USING POST : "api/auth/addnote". Login required
+// ROUTE 2: ADD A NEW NOTE USING POST : "api/notes/addnote". Login required
 router.post(
   "/addnote",
   fetchuser,
@@ -45,10 +45,41 @@ router.post(
 
       res.json(savedNote);
     } catch (error) {
-      res.status(401),
+      res.status(401).
         send({ error: "Please authenticate using a valid token" });
     }
   }
 );
+
+// ROUTE 3: UPDATE A NOTE : "api/notes/updatenote. Login required"
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+  const newNote = {};
+  if (title) {
+    newNote.title = title;
+  }
+  if (description) {
+    newNote.description = description;
+  }
+  if (tag) {
+    newNote.tag = tag;
+  }
+
+  // find the note to be updated ad update it
+  let note = await Notes.findById(req.params.id);
+  if (!note) {
+    return res.status(404).send("Not Found");
+  }
+  if (note.user.toString() !== req.user.id) {
+    return res.status(401).send("Not Allowed");
+  }
+
+  note = await Notes.findByIdAndUpdate(
+    req.params.id,
+    { $set: newNote },
+    { new: true }
+  );
+  res.json({ note });
+});
 
 module.exports = router;
